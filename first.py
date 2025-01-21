154eb42838bae6d90a8ed17400938e259d1e7826
prajwal6394 <prajwalpandey2009@gmail.com>
2025-01-22 01:30:23 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,16 @@
+# print('Hello man');
+# print('what is your name');
+
+# name = input();
+
+# print('Hello ' + name);
+# print('length of your name is ' + str(len(name)));
+
+# print('what is your age');
+# age = input();
+# print('you will be ' + str(int(age) + 1) + ' in a year');
+
+i = 1
+for i in range(1, 11):
+    print(i)
+    i += 1
