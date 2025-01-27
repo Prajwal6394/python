6059e9662d19631c366e97ed9bfed0b8fef65a91
prajwal6394 <prajwalpandey2009@gmail.com>
2025-01-28 01:32:29 +0530

    intro list

--- first.py
+++ first.py
@@ -135,27 +135,35 @@
 #     print('You did not enter a number');
 
 
-import random
+# import random
 
-print('Hello! What is your name?');
-name = input();
+# print('Hello! What is your name?');
+# name = input();
 
-print('Well, ' + name + ', I am thinking of a number between 1 and 20');
+# print('Well, ' + name + ', I am thinking of a number between 1 and 20');
 
-secretNumber = random.randint(1, 20);
+# secretNumber = random.randint(1, 20);
 
-for guessesTaken in range(1, 7):
-    print('Take a guess');
-    guess = int(input());
+# for guessesTaken in range(1, 7):
+#     print('Take a guess');
+#     guess = int(input());
 
-    if guess < secretNumber:
-        print('Your guess is too low');
-    elif guess > secretNumber:
-        print('Your guess is too high');
-    else:
-        break;
+#     if guess < secretNumber:
+#         print('Your guess is too low');
+#     elif guess > secretNumber:
+#         print('Your guess is too high');
+#     else:
+#         break;
 
-if guess == secretNumber:
-    print('Good job, ' + name + '! You guessed my number in ' + str(guessesTaken) + ' guesses');
-else:
-    print('Nope. The number I was thinking of was ' + str(secretNumber));
+# if guess == secretNumber:
+#     print('Good job, ' + name + '! You guessed my number in ' + str(guessesTaken) + ' guesses');
+# else:
+#     print('Nope. The number I was thinking of was ' + str(secretNumber));
+
+
+# array = ['cal', 'cat', 'bat', 'rat', 'mat', 'fat', 'sat', 'pat', 'hat'];
+# print(array[0:3]);
+
+
+arrayInsideArray = [['cal', 'cat', 'bat'], ['rat', 'mat', 'fat'], ['sat', 'pat', 'hat']];
+print(arrayInsideArray[1][1]);
