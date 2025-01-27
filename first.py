19bf7b04ab203812227ceea1b586f6cc0d6d529c
prajwal6394 <prajwalpandey2009@gmail.com>
2025-01-28 01:41:33 +0530

    Add loops to print indices and elements of supplies list

--- first.py
+++ first.py
@@ -177,3 +177,15 @@ print(res);
 
 res2 = 'howsdy' not in ['hello', 'hi', 'howdy', 'hey'];
 print(res2);
+
+
+for i in range(4):
+    print(i);
+
+for i in [0, 1, 2, 3]:
+    print(i);
+
+supplies = ['pens', 'staplers', 'flame-throwers', 'binders'];
+
+for i in range(len(supplies)):
+    print('Index ' + str(i) + ' in supplies is: ' + supplies[i]);
