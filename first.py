f0c772eeac10d32a913183076432373b2b01aac7
prajwal6394 <prajwalpandey2009@gmail.com>
2025-01-28 01:23:33 +0530

    random guess program

--- first.py
+++ first.py
@@ -124,12 +124,38 @@
 
 # with error handling
 
-try:
-    print('How many cats do you have?');
-    numCats = input();
-    if int(numCats) >= 4:
-        print('That is a lot of cats');
-    else:   
-        print('That is not that many cats');
-except ValueError:
-    print('You did not enter a number');
+# try:
+#     print('How many cats do you have?');
+#     numCats = input();
+#     if int(numCats) >= 4:
+#         print('That is a lot of cats');
+#     else:   
+#         print('That is not that many cats');
+# except ValueError:
+#     print('You did not enter a number');
+
+
+import random
+
+print('Hello! What is your name?');
+name = input();
+
+print('Well, ' + name + ', I am thinking of a number between 1 and 20');
+
+secretNumber = random.randint(1, 20);
+
+for guessesTaken in range(1, 7):
+    print('Take a guess');
+    guess = int(input());
+
+    if guess < secretNumber:
+        print('Your guess is too low');
+    elif guess > secretNumber:
+        print('Your guess is too high');
+    else:
+        break;
+
+if guess == secretNumber:
+    print('Good job, ' + name + '! You guessed my number in ' + str(guessesTaken) + ' guesses');
+else:
+    print('Nope. The number I was thinking of was ' + str(secretNumber));
