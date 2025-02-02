12edabc93fe415a67678c2f11222ead8a36cb436
prajwal6394 <prajwalpandey2009@gmail.com>
2025-02-02 20:16:02 +0530

    Add variable unpacking and print statements for cat and swap values of a and b

--- first.py
+++ first.py
@@ -189,3 +189,22 @@ supplies = ['pens', 'staplers', 'flame-throwers', 'binders'];
 
 for i in range(len(supplies)):
     print('Index ' + str(i) + ' in supplies is: ' + supplies[i]);
+
+cat = ['fat', 'orange', 'loud'];
+
+size = cat[0];
+color = cat[1];
+disposition = cat[2];
+
+print(size);
+
+size, color, disposition = cat;
+
+
+a = 'AAA';
+b = 'BBB';
+
+a, b = b, a;
+
+print(a);
+print(b);
